b6dedfb949f848e40f125f18a1df9772dee453be
juliocesarsmelo <jcesarsm17@gmail.com>
2021-11-06 20:56:47 -0300

    ==index + alteracao no codigo HTML

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Yep Imports - Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="public/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+</head>
+<body>
+	
+</body>
+</html>
