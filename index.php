c2e6d5ff1cbdcb4c1c09a838bc37751c5797650e
juliocesarsmelo <jcesarsm17@gmail.com>
2021-11-07 11:58:30 -0300

    ==index + formulario de login

--- index.php
+++ index.php
@@ -4,11 +4,23 @@
 	<title>Yep Imports - Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="public/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="./public/images/icons/favicon.ico"/>
+	<!--===============================================================================================-->
 </head>
 <body>
+
+	<form action="./app/verificarLogin.php" method="POST">
+		<label for="email">Email:</label>
+		<input type="email" name="email">
+		<label for="senha">Senha:</label>
+		<input type="password" name="senha">
+
+		<input type="submit" value="Entrar">
+
+		<p>Não possui login? <a href="./public/cadastro.php">Cadastre-se</a></p>
+		
+	</form>
 	
 </body>
 </html>
