fec1ce320555bb9e7be55a986852919d798bd673
juliocesarsmelo <jcesarsm17@gmail.com>
2021-11-07 15:04:02 -0300

    ==public + implementação de controle de sessão na home.php

--- public/home.php
+++ public/home.php
@@ -1,8 +1,24 @@
 <?php
+    
+    // Iniciando sessão ou resumindo sessão existe 
+    session_start(); 
 
-    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
-        session_unset(); // remove todas as variáveis de sessão
-        session_destroy(); // destroi a sessão
+    if(!isset($_SESSION["email"]) || !isset($_SESSION["nome"])){
+
+        $_SESSION = array();
+
+        // Apagando os cookies de sessão
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        // Destruindo a sessão
+        session_destroy();
+
         echo "<script>
                 alert('Está página só pode ser acessa por usuário logado');
                 window.location.href='../index.php';
@@ -13,14 +29,15 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Yep Imports - Home</title>
 </head>
 <body>
-    <a href="../index.php">Fazer Logout</a>
+    <h1>Olá, <?= $_SESSION['nome'] ?></h1>
+    <a href="../app/logout.php">Fazer Logout</a>
 </body>
 </html>
