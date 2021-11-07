76450fb8b342b6f0658fb7ad6384c9bd7d7720ae
juliocesarsmelo <jcesarsm17@gmail.com>
2021-11-07 19:50:37 -0300

    ==public + alteracao no html de cadastro.php

--- public/cadastro.php
+++ public/cadastro.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<title>Yep Imports - Login</title>
+	<title>Yep Imports - Cadastro</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->	
