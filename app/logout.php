5d469208fe580b4eafa6e00bebc3150e9c5d57c3
juliocesarsmelo <jcesarsm17@gmail.com>
2021-11-07 15:02:14 -0300

    ==app + implementação no sistema de logout

--- app/logout.php
+++ app/logout.php
@@ -1,7 +1,20 @@
 <?php 
 
-    session_start(); //  Iniciando sessão ou resumindo sessão existe   
+    // Iniciando sessão ou resumindo sessão existe   
+    session_start(); 
 
-    session_unset(); // remove todas as variáveis de sessão
-    session_destroy(); // destroi a sessão
+    $_SESSION = array();
+
+    // Apagando os cookies de sessão
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    // Destruindo a sessão
+    session_destroy();
+
     header("Location: ../index.php");
