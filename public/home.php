e23a7f5f0dcd0884a339bce45d4b78d0294e06f2
juliocesarsmelo <jcesarsm17@gmail.com>
2021-11-07 22:22:44 -0300

    ==public + adicao de favicon

--- public/home.php
+++ public/home.php
@@ -45,6 +45,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="./images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
     <title>Yep Imports - Home</title>
 </head>
 <body>
